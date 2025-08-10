6cdeceea5ce852697d0850f5926c364a65dc4ac4
aryanmestry07 <aryanmestry07@gmail.com>
2025-08-10 20:19:34 +0530

    Added arithmetic operations program

---
+++ arithmetic_operations.py
@@ -0,0 +1,10 @@
+# Write a Python program that asks the user for two numbers 
+# and prints the sum, difference, product, and quotient of those numbers.
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)
