57976f7f0345aab9fffad6f32e62d46c892e1d75
Aryan Mestry <aryanmestry07@gmail.com>
2025-10-05 14:41:57 +0530

    Create mul_of_ele_in_list.py

---
+++ mul_of_ele_in_list.py
@@ -0,0 +1,9 @@
+#Q) WAP in python which will print multiplication of all elements in the given list 
+
+my_list = [4,5]
+mul = 1
+
+for i in my_list:
+    mul = mul*i;
+
+print(f"The Multiplication of all the Elements in List is : {mul}")
